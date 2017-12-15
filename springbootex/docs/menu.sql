d6e1a4efda4820368c103a09cdb9a1187eabf969
SUBEENKIM <subeen9313@gmail.com>
2017-12-15 10:31:06 +0900

    '.'

--- springbootex/docs/menu.sql
+++ springbootex/docs/menu.sql
@@ -24,6 +24,6 @@ alter table branch_master add unique key(bmemail);
 
 desc branch_master;
 
-insert into branch_master(bmno, bmemail, bmpwd, bmname, bmtel, bmfbid, bmgrade) values(1, 'a1@test.com', password('1111'), 'hong1', '111-1111', 'a1@test.com', 'branch_master')
-insert into branch_master(bmno, bmemail, bmpwd, bmname, bmtel, bmfbid, bmgrade) values(2, 'a2@test.com', password('1111'), 'hong2', '222-2222', 'a2@test.com', 'branch_master')
-insert into branch_master(bmno, bmemail, bmpwd, bmname, bmtel, bmfbid, bmgrade) values(3, 'a3@test.com', password('1111'), 'hong3', '333-3333', 'a3@test.com', 'pre_master')
+insert into branch_master(bmno, bmemail, bmpwd, bmname, bmtel, bmfbid, bmgrade) values(1, 'a1@test.com', password('1111'), 'hong1', '111-1111', 'a1@test.com', 'branch_master');
+insert into branch_master(bmno, bmemail, bmpwd, bmname, bmtel, bmfbid, bmgrade) values(2, 'a2@test.com', password('1111'), 'hong2', '222-2222', 'a2@test.com', 'branch_master');
+insert into branch_master(bmno, bmemail, bmpwd, bmname, bmtel, bmfbid, bmgrade) values(3, 'a3@test.com', password('1111'), 'hong3', '333-3333', 'a3@test.com', 'pre_master');
